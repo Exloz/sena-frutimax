266723fce9436df4abdde5665eb391e6496d87dc
Exloz <exloz@hotmail.com>
2024-11-14 22:02:48 -0500

    se creo clase Cliente

---
+++ src/main/java/com/sena/frutimax/modelo/Cliente.java
@@ -0,0 +1,11 @@
+package com.sena.frutimax.modelo;
+
+import java.util.Date;
+
+public class Cliente {
+    String nombre;
+    String apellido;
+    String cedula;
+    String telefono;
+    Date fechaDeNacimiento;
+}
