9980cdf156fa30c28142ea249b9d30ea451afec8
Exloz <exloz@hotmail.com>
2024-11-14 21:49:09 -0500

    commit inicial

---
+++ src/main/java/com/sena/frutimax/FrutimaxApplication.java
@@ -0,0 +1,13 @@
+package com.sena.frutimax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrutimaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrutimaxApplication.class, args);
+	}
+
+}
