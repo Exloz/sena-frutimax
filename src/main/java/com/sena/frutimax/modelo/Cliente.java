6942ed6e87fccd71f8b5931b2a6a89bd88826e8b
Exloz <exloz@hotmail.com>
2024-11-17 18:15:51 -0500

    añadir anotaciones y getters y setters

--- src/main/java/com/sena/frutimax/modelo/Cliente.java
+++ src/main/java/com/sena/frutimax/modelo/Cliente.java
@@ -1,11 +1,68 @@
 package com.sena.frutimax.modelo;
 
-import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cliente {
-    String nombre;
-    String apellido;
-    String cedula;
-    String telefono;
-    Date fechaDeNacimiento;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nombre;
+    private String cedula;
+    private String contacto;
+    private String historialDePedidos;
+    private Boolean activo;
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getHistorialDePedidos() {
+        return historialDePedidos;
+    }
+
+    public void setHistorialDePedidos(String historialDePedidos) {
+        this.historialDePedidos = historialDePedidos;
+    }
 }
